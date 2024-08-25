aa14259a4f655901b7205bfde922e6cb9387a9b6
unknown <sigitnj89@gmail.com>
2024-08-25 20:58:19 +0700

    merged

--- master/src/services/auth.service.js
+++ master/src/services/auth.service.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-<<<<<<< HEAD
+import jwt_decode from 'jwt-decode'
 
 export const login = (data) => {
   axios.post('https://fakestoreapi.com/auth/login', data).then((res)=>{
@@ -7,19 +7,11 @@ export const login = (data) => {
   }).catch((err) => {
     console.log(err)
   })
-=======
+}
 import jwt_decode from 'jwt-decode'
 
-export const login = (data, callback) => {
-    axios.post('https://fakestoreapi.com/auth/login', data).then((res) => {
-        callback(true, res.data.token)
-    }).catch ((err)=> {
-        callback(false, err)
-    })
-}
 export const getUsername = (token) => {
     const decoded = jwt_decode(token)
     console.log(decoded)
     return decoded.user
->>>>>>> 0051938497877f77d88952b170f9d3faa3c55105
 }
