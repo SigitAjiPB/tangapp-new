07377af859528d73c0b12d0fc69c5b79e1952908
RifqiNotWibu <rifqifadhillahh@gmail.com>
2024-11-18 16:27:55 +0700

    Modified login function

--- master/src/services/auth.service.js
+++ master/src/services/auth.service.js
@@ -1,22 +1,16 @@
-import axios from 'axios'
-import {jwtDecode} from 'jwt-decode'
+// import axios from 'axios'
+import { jwtDecode } from 'jwt-decode'
 
-export const login = (data, callback) => {
-  axios
-  .post('https://fakestoreapi.com/auth/login', data)
-  .then((res)=>{
-    callback(true, res.data.token)
-  })
-  .catch((err) => {
-    callback(false, err)
-  })
+export const login = () => {
+  // Redirect the user to the backend's Google OAuth endpoint
+  window.location.href = 'http://localhost:8080/api/auth/google'
 }
 
 export const getUsername = (token) => {
-    const decoded = jwtDecode(token)
-    return decoded.user
+  const decoded = jwtDecode(token)
+  return decoded.name
 }
 export const getIat = (token) => {
-    const decoded = jwtDecode(token)
-    return decoded.iat
+  const decoded = jwtDecode(token)
+  return decoded.exp
 }
