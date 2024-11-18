489022148227cf47ac001844db679887837a4edd
RifqiNotWibu <rifqifadhillahh@gmail.com>
2024-11-18 16:29:08 +0700

    Added new route for login pop-up

--- master/src/App.jsx
+++ master/src/App.jsx
@@ -1,43 +1,41 @@
-
-import LoginPage from "./Pages/login"
-import { Routes, Route, Outlet } from "react-router-dom"
-import RegisterPage from "./Pages/register"
-import HomePage from "./Pages/home"
-import BillingPage from "./Pages/billings"
-import EventFormPage from "./Pages/eventform"
-import ProfilePage from "./Pages/profile"
-import DetailEventPage from "./Pages/detailEvent"
-import SettingPage from "./Pages/settings"
-import UserPage from "./Pages/user"
+import LoginPage from './Pages/login'
+import { Routes, Route, Outlet } from 'react-router-dom'
+import RegisterPage from './Pages/register'
+import HomePage from './Pages/home'
+import BillingPage from './Pages/billings'
+import EventFormPage from './Pages/eventform'
+import ProfilePage from './Pages/profile'
+import DetailEventPage from './Pages/detailEvent'
+import SettingPage from './Pages/settings'
+import UserPage from './Pages/user'
+import AuthCallback from './Pages/authcallback'
 
 function Dashboard() {
   return (
-    <div className="wrapper">
+    <div className='wrapper'>
       <HomePage>
-        <Outlet/>
+        <Outlet />
       </HomePage>
     </div>
   )
 }
 
-
 function App() {
-
   return (
     <>
       <Routes>
-        <Route path="/" element={<Dashboard/>}>
-          <Route path="billings" element={<BillingPage/>} />
-          <Route path="eventform" element={<EventFormPage />} />
-          <Route path="settings" element={<SettingPage />} />
-          <Route path="/detail/:id" element={<DetailEventPage />} />
-          <Route path="/user" element={<UserPage />} />
+        <Route path='/' element={<Dashboard />}>
+          <Route path='billings' element={<BillingPage />} />
+          <Route path='eventform' element={<EventFormPage />} />
+          <Route path='settings' element={<SettingPage />} />
+          <Route path='/detail/:id' element={<DetailEventPage />} />
+          <Route path='/user' element={<UserPage />} />
         </Route>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/register' element={<RegisterPage />} />
+        <Route path='/profile' element={<ProfilePage />} />
+        <Route path='/auth-callback' element={<AuthCallback />} />
       </Routes>
-
     </>
   )
 }
